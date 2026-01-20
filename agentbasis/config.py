12376b98cff04be9221f39eef305837881296800
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2026-01-19 21:42:34 -0800

    removed api url option from config.py

--- agentbasis/config.py
+++ agentbasis/config.py
@@ -4,12 +4,11 @@ from typing import Optional
 class Config:
     """
     Configuration settings for the AgentBasis SDK.
-    Handles API keys and endpoint URLs.
+    Handles API keys and Agent IDs.
     """
-    def __init__(self, api_key: Optional[str] = None, api_url: Optional[str] = None, agent_id: Optional[str] = None):
+    def __init__(self, api_key: Optional[str] = None, agent_id: Optional[str] = None):
         self.api_key = api_key or os.environ.get("AGENTBASIS_API_KEY") # Get API Key from the arguments first, if not provided, try to get it from the environment variables.
         self.agent_id = agent_id or os.environ.get("AGENTBASIS_AGENT_ID") # Get Agent ID from the arguments first, if not provided, try to get it from the environment variables.
-        self.api_url = api_url or os.environ.get("AGENTBASIS_API_URL", "https://api.agentbasis.co") # Set the API URL (defaulting to the hosted version if not changed, https://api.agentbasis.co is the hosted version)
 
     def validate(self):
         """
